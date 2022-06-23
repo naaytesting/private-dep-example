4d6a48b81072ec7354bf93666e84256146c55758
Miguel Jimeno <miguelangel4b@gmail.com>
2022-06-23 11:40:48 +0000

    all: import project with private dependencies

---
+++ index.js
@@ -0,0 +1,19 @@
+const https = require('https');
+const isok = require ('@naaytesting/isok');
+
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/',
+  method: 'GET',
+};
+
+const req = https.request(options, res => {
+  console.log(`Is HTTP response status a 200 OK? ${isok.check(res)}`);
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.end();
